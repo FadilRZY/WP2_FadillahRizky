652534b21f3b7953d7511adc02dbc496f250011f
FadilRZY <93172694+FadilRZY@users.noreply.github.com>
2021-10-29 10:41:07 +0700

    base url
    mengganti base url

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class contoh1 extends CI_Controller
+
+{
+    public function index()
+    {
+        echo"<h1>perkenalkan</h1>
+        echo"nama saya fadillah Rizky
+        saya tinggal di daerah cikampek olah raga 
+        yang saya sukai voli";
+    }
+}
